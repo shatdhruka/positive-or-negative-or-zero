83c974f0bbd5a36430c1f4083d528e972b9b9095
shatdhruka <42232515+shatdhruka@users.noreply.github.com>
2018-09-06 11:43:32 +0530

    Create PosNegZero.java

---
+++ PosNegZero.java
@@ -0,0 +1,24 @@
+# positive-or-negative-or-zero
+import java.util.*;
+ 
+class PosNegZero
+{
+    public static void main(String []args)
+    {
+        int num;
+        //Scanner class to read value
+        Scanner sc=new Scanner(System.in);
+         
+        System.out.print("Enter any integer number: ");
+        num=sc.nextInt();
+         
+        //check condition for +ve, -ve and Zero
+        if(num>0)
+            System.out.println(num + " is POSITIVE NUMBER.");
+        else if(num<0)
+            System.out.println(num + " is NEGATIVE NUMBER.");
+        else
+            System.out.println("IT's ZERO.");
+         
+    }
+}
