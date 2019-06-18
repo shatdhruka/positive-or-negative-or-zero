1a9fb38ac2ebd40d7116742557e7c246114e0c05
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-18 17:40:35 +0530

    Update PosNegZero.java

--- PosNegZero.java
+++ PosNegZero.java
@@ -5,20 +5,16 @@ class PosNegZero
 {
     public static void main(String []args)
     {
-        int num;
-        //Scanner class to read value
+        int z;
         Scanner sc=new Scanner(System.in);
+        z=sc.nextInt();
          
-        System.out.print("Enter any integer number: ");
-        num=sc.nextInt();
-         
-        //check condition for +ve, -ve and Zero
-        if(num>0)
-            System.out.println(num + " is POSITIVE NUMBER.");
-        else if(num<0)
-            System.out.println(num + " is NEGATIVE NUMBER.");
+        if(z>0)
+            System.out.println("Positive");
+        else if(z<0)
+            System.out.println("Negative");
         else
-            System.out.println("IT's ZERO.");
+            System.out.println("Zero");
          
     }
 }
